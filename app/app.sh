904eb6e4baf899c1e0f46b68936b4e0d224a19dc
Brett Kuskie <fullaxx@gmail.com>
2025-05-26 04:42:15 -0600

    Add app/set_gateway.sh

--- app/app.sh
+++ app/app.sh
@@ -10,6 +10,10 @@ source /app/imagestart.sh
 
 /app/qbtcheck.sh
 
+if [ -n "${GATEWAY}" ]; then
+  /app/set_gateway.sh ${GATEWAY}
+fi
+
 if [ x"${OVPNCFG}" != "x" ]; then
   /app/openvpn.sh || bail "openvpn startup failed!"
   if [ x"${OVPNKS}" != "x" ]; then
